2c234ac7cf5f10bf02fdf638a22e1d1d8c33259b
Vuk Radosavljevic <vuk@Vuks-iMac.local>
2019-01-02 16:50:40 -0600

    First commit

---
+++ Facial Exercises/Facial Exercises/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facial Exercises
+//
+//  Created by Vuk Radosavljevic on 1/2/19.
+//  Copyright © 2019 Vuk Radosavljevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
